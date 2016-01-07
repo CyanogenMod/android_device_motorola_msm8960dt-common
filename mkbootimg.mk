8b6d4e85c2c9112422276311576b2b135930948d
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-01-07 10:07:17 +0000

    msm8960dt-common: fix recovery build
    * After 8e0ac24 android_build
    
    Change-Id: I6a645de4cad1e2148ba26c55961bfbaeab8b003c

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -52,7 +52,7 @@ bootimage-nodeps: $(MKBOOTIMG)
 	@echo -e ${CL_INS}"Made boot image: $@"${CL_RST}
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) $(recovery_ramdisk) $(recovery_kernel)
-	$(call pretty,"Target recovery image: $@")
+	$(call build-recoveryimage-target, $@)
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE))
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
